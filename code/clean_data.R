e0e0c353214382c9ef3ed78c9fabf4ca47f5112c
Nick Lesniak <NLesniak@umich.edu>
2018-12-21 12:00:40 -0500

    remove long form clean up

--- code/clean_data.R
+++ code/clean_data.R
@@ -29,11 +29,7 @@ seq_sample_names <- bind_rows(
 		mutate(read_xlsx(seq_plate_map, sheet = 'Final Plate Map', range = 'C27:O35'), plate = 3),
 		mutate(read_xlsx(seq_plate_map, sheet = 'Final Plate Map', range = 'C39:O47'), plate = 4)) %>%
 	rename(row = X__1) %>%
-	gather(column, seq_sample, -row, -plate) %>%
-	mutate(seq_day = as.numeric(str_match(seq_sample, '(?<=D)\\d{1,2}')),
-		seq_mouse = as.numeric(str_match(seq_sample, '(?<=M)\\d{1,2}')),
-		seq_treatment = str_match(seq_sample, '[:alpha:]{1,2}(?=pos)'),
-		seq_sample_id = paste(seq_treatment, seq_mouse, seq_day, sep = '_'))
+	gather(column, seq_sample, -row, -plate)
 # load in all the names and positions of the extraction plates
 extraction123 <- 'data/raw/Extraction Template_Plate #1#2#3.xlsx'
 extraction4 <- 'data/raw/Plate_4_Sarah_Omep2Box1.xlsx'
@@ -64,34 +60,17 @@ fixed_shared_names <- full_join(seq_sample_names, ext_sample_names, by = c('row'
 	inner_join(shared_sample_names, by = c('shared_names' = 'Group')) %>%
 	mutate(new_shared_names = paste0(ext_treatment, 'POS+M', ext_mouse, '+D', ext_day)) %>%
 	select(shared_names, new_shared_names)
-
+# join shared sample names with corrected sample names
 shared_sample_names <- shared_sample_names %>%
 	full_join(fixed_shared_names, by = c('Group' = 'shared_names')) %>%
 	mutate(shared_samples = ifelse(is.na(new_shared_names), Group, new_shared_names))
 
 sample_id_df <- shared_sample_names %>% 
-	mutate(sample_id = gsub("\\+", "\\_", group), # change separator, don't necessarily need to
-		sample_id = gsub('^M', 'NAPOS_M', sample_id)) %>% # add NA to samples missing label treatment
-	filter(grepl('POS', sample_id)) %>% # remove sequencing or unlabeled samples
-	mutate(label_error = grepl('UNK', sample_id), # create column to mark samples that have unknown parts of their label
-		sample_id = gsub('_UNK', '', sample_id)) %>% # remove UNK since moved unknonwn label to new column
- 	split(., grepl('^D', .$sample_id)) # create label to differentiate the samples with different ordering of info
-# separated the piping because we have to split the data and process each group differently based on the order of the group id
-sample_id_df <- rbind(sample_id_df$`TRUE` %>% 
-		separate(sample_id, c('day', 'treatment', 'mouse')) %>% 
-		select(group, mouse, day, treatment, label_error), # make sure columns in both sections are in the same order
-	sample_id_df$`FALSE` %>% 
-		separate(sample_id, c('treatment', 'mouse', 'day')) %>% 
-		select(group, mouse, day, treatment, label_error) # make sure columns in both sections are in the same order 
-		) %>% 
-	mutate(mouse = as.numeric(gsub('M', '', mouse)), # remove prefix and convert to numeric for day and mouse number
-		day = as.numeric(gsub('D', '', day)),
-		treatment = gsub('POS', '', treatment),
-		treatment = gsub('NA', NA,treatment)) %>%  # remove POS label since all samples are were challenged with C diff (pos)
-	### remove all samples with known labeling issues (remove once addressed)
-	# identify rows which didnt have a day, mouse or treatment in the label
-	mutate(label_error = apply(., 1, function(x)any(is.na(x))) == T | label_error == T) # %>% 
-	# filter(label_error == F) 
+	mutate(day = as.numeric(str_match(shared_samples, '(?<=D)\\d{1,2}')),
+		mouse = as.numeric(str_match(shared_samples, '(?<=M)\\d{1,2}')),
+		treatment = as.character(str_match(shared_samples, '[:alpha:]{1,2}(?=POS)')),
+		sample_id = paste(treatment, mouse, day, sep = '_')) %>%
+	filter(treatment %in% c('C', 'CO', 'O'))
 
 # load the metafile originally recording the data to check out sequence labels against
 true_data <- readxl::read_xlsx('data/raw/PPI_Exp_Sample_List_2018.xlsx', sheet = 'Sheet1') %>% 
