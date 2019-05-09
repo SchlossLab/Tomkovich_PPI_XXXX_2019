28c1e9f9ab15f712df898dd0f8dabfcdf863fb2e
Sarah Tomkovich <tomkoset@umich.edu>
2019-05-09 14:03:08 -0400

    Incorporate comments, etc. from code review

--- code/ppi_taxa.R
+++ code/ppi_taxa.R
@@ -14,41 +14,42 @@
 #		
 #
 #####################
-
+#Setup----
 library(tidyverse)
 
-# Import metadata into data frame
+# Import metadata into data frame and clean
 metadata <- read.table('data/process/ppi_metadata.txt', header = T, sep = '\t', stringsAsFactors = F) %>% 
-  filter(Group != "NA") %>% 
-  mutate(Group = case_when(Group == 'O+' ~ 'PPI', 
+  filter(Group != "NA") %>% #Exclude the mock community
+  mutate(Group = case_when(Group == 'O+' ~ 'PPI', #make group names readable by humans
                            Group == 'C+' ~ 'Clindamycin',
                            Group == 'CO+' ~ 'Clindamycin + PPI'))
 
-# Import taxonomy into data frame
+# Import taxonomy into data frame and clean up taxonomy names
 taxonomy <- read_tsv(file="data/mothur/ppi.taxonomy") %>%
-  rename_all(tolower) %>% 
+  rename_all(tolower) %>% #remove uppercase from column names
 # Split taxonomic information into separate columns for each taxonomic level  
-  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="\\(\\d*\\)", replacement="")) %>%
-  mutate(taxonomy=str_replace_all(string=taxonomy, pattern=";$", replacement="")) %>%
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern="\\(\\d*\\)", replacement="")) %>% #drop digits with parentheses around them
+  mutate(taxonomy=str_replace_all(string=taxonomy, pattern=";$", replacement="")) %>% #removes semi-colon at end of line
   mutate(taxonomy=str_replace_all(string=taxonomy, pattern="Bacteria_unclassified", replacement="Unclassified")) %>% 
-# Separate taxonomic levels into separate columns.
+# Separate taxonomic levels into separate columns according to semi-colon.
   separate(taxonomy, into=c("kingdom", "phylum", "class", "order", "family", "genus"), sep=';')
 
 
 #Look at rarefaction curves, import rarefaction data frame
 rarefy <- read_tsv(file="data/mothur/ppi.opti_mcc.groups.rarefaction") %>% 
   select(-contains("lci-"), -contains("hci-")) %>% 
-  gather(-numsampled, key=sample, value=sobs) %>% 
-  mutate(sample=str_replace_all(sample, patter="0.03-", replacement="")) %>% 
+  gather(-numsampled, key=sample, value=sobs) %>% #not including numsampled, gather samples intow two columns
+  mutate(sample=str_replace_all(sample, patter="0.03-", replacement="")) %>% #drop prefix 0.03
   drop_na()
 
 #Join metadata to rarefy data frame
 metadata_rarefy <- inner_join(metadata, rarefy, by = c("shared_names" = "sample")) %>% 
   filter(Group != "NA") 
 
+#Plotting----
 #Plot rarefaction curves, the more parallel the curves to the x axis, the more confident you can be in the results
 ggplot(metadata_rarefy, aes(x=numsampled, y=sobs, group=shared_names, color=Group))+
-  geom_vline(xintercept=1000, color="gray", size=2) +
+  geom_vline(xintercept=3000, color="gray", size=2) +
   geom_line()+
   coord_cartesian(xlim=c(0, 10000), ylim=c(0,200))+
   labs(subtitle="Vertical line indicates the number of sequences that samples were rarefied to",
@@ -62,7 +63,7 @@ otu_data <- read_tsv("data/mothur/ppi.opti_mcc.0.03.subsample.shared", col_types
   select(-label, -numOtus) %>% 
   rename(shared_names=Group) %>% 
   gather(-shared_names, key="otu", value="count") %>% 
-  mutate(rel_abund=count/1000) 
+  mutate(rel_abund=count/3000) 
 
 # Merge otu_data to taxonomy data frame
 agg_taxa_data <- inner_join(otu_data, taxonomy) 
@@ -89,7 +90,8 @@ agg_phylum_data %>%
   mutate(agg_rel_abund = agg_rel_abund + 1/2000) %>% 
   ggplot(aes(x= reorder(phylum, -agg_rel_abund), y=agg_rel_abund, color=Group))+
   geom_hline(yintercept=1/1000, color="gray")+
-  geom_boxplot()+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(shape=19, size=1, alpha=0.7, position=position_jitterdodge(dodge.width=0.7, jitter.width=0.2)) +
   labs(title=NULL, 
        x=NULL,
        y="Relative abundance (%)")+
@@ -116,9 +118,10 @@ top_genus <- agg_genus_data %>%
 agg_genus_data %>% 
   filter(genus %in% top_genus) %>% 
   mutate(agg_rel_abund = agg_rel_abund + 1/2000) %>% 
-  ggplot(aes(x= reorder(genus, -agg_rel_abund), y=agg_rel_abund, color=Group))+
+  ggplot(aes(x= reorder(genus, -agg_rel_abund), y=agg_rel_abund, color=Group)) +
   geom_hline(yintercept=1/1000, color="gray")+
-  geom_boxplot()+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(shape=19, size=1, alpha=0.7, position=position_jitterdodge(dodge.width=0.7, jitter.width=0.1)) +
   labs(title=NULL, 
        x=NULL,
        y="Relative abundance (%)")+
