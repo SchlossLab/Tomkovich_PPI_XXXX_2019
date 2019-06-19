935deb62e7b0c9b54175943c2bc990a154310c3b
Sarah Tomkovich <tomkoset@umich.edu>
2019-06-19 14:05:52 -0400

    Rename group names and subtract 7 from day column to match experiment setup where C. difficile is administered on day 0

--- code/merge_labels.R
+++ code/merge_labels.R
@@ -33,7 +33,10 @@ metadata <- read_xlsx('data/raw/PPI_metadata.xlsx') %>%
   rename("Plate_label" = "Plate map label") %>% 
   # convert names of seq to match the format of the shared samples
   mutate(shared_names = gsub('^_{1,3}','', Plate_label),
-         shared_names = gsub('_', '+', shared_names)) 
+         shared_names = gsub('_', '+', shared_names)) %>% 
+  mutate(Group = case_when(Group == 'O+' ~ 'PPI', #make group names more readable
+                           Group == 'C+' ~ 'Clindamycin',
+                           Group == 'CO+' ~ 'Clindamycin + PPI')) 
   # select only the samples that are present in the shared data
 matching <- inner_join(metadata, shared_sample_names, by = c('shared_names' = 'Group')) 
   
@@ -58,6 +61,10 @@ ppi_cdiff_cfu <- metadata %>%
 #Join CFU data to metadata file
 output_df <- full_join(metadata,
                        ppi_cdiff_cfu, by = c('Mouse ID', 'day'))
+
+#Change day to match experimental setup where C. difficile challenge day equals day 0. Implement by subtracting 7 from each day.
+output_df <- output_df %>% 
+  mutate(day = day - 7)
 
 write.table(output_df, 'data/process/ppi_metadata.txt', 
   row.names = F, quote = F, sep = '\t')
