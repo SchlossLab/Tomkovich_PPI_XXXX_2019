25b57cf73ef21e2ff468d6e025536024741b1be5
Sarah Tomkovich <tomkoset@umich.edu>
2019-07-16 11:01:29 -0400

    Add additional columns to satisfy NCBI's sample uniqueness requirement

--- code/mimarks_ncbi_info.R
+++ code/mimarks_ncbi_info.R
@@ -9,7 +9,11 @@ metadata <- read.table('data/process/ppi_metadata.txt', header = T, sep = '\t', 
   unite(sample_title, Tube.label, Tube.label__1) %>% 
   mutate(description = Group) %>% 
   mutate(collection_date = excel_numeric_to_date(Date.Collected)) %>% #Convert excel numeric values to date values with excel_numeric_to_date function from the janitor package
-  select(sample_title, description, collection_date, shared_names) #Select columns that are missing from mimarks tsv file
+  mutate(experiment_day = day, #Reformat names of additional columns since they'll be included in mimarks file so that each sample will pass NCBI's uniqueness check.
+         sex = M.F,
+         exp_cage_number = Exp..cage,
+         mouse_id = Mouse.ID) %>% 
+  filter(sample_title, description, collection_date, experiment_day, sex, mouse_id, exp_cage_number, shared_names) #Select columns that are missing from mimarks tsv file
 
 
 # Import MIMARKS ppi.tsv file into data frame
@@ -17,5 +21,7 @@ mimarks <- read_tsv('data/raw/ppi.tsv')
 # Add metadata columns to mimarks file to finish filling out the information for all samples
 mimarks <- left_join(mimarks, metadata, by = c("#This is a tab-delimited file. Additional Documentation can be found at http://www.mothur.org/wiki/MIMarks_Data_Packages." = "shared_names"))
 write_tsv(mimarks, 'data/raw/ppi.tsv', na = " ")
+
+# To prepare for NCBI submission. Open up file in excel and move additional column names to their appropriate location on row 10
           
                      
