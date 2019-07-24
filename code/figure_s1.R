961e36d184c5c4f084148757bb34c7601e0e653b
Sarah Tomkovich <tomkoset@umich.edu>
2019-07-24 17:15:47 -0400

    Fix sizing

--- code/figure_s1.R
+++ code/figure_s1.R
@@ -8,7 +8,7 @@ c <- ggdraw() + draw_image("results/figures/ppi_genera_time.png")
 
 plot_grid(a, b, c, labels = "AUTO", label_size = 12)+
   ggsave("results/figures/figure_s1.pdf", width=6.5, height=3)+
-  ggsave("submission/figure_s1.pdf", width=6.5, height=3.5)
+  ggsave("submission/figure_s1.pdf", width=6.5, height=3)
 
 
 
