4f97e863d386facfe93187d358221f67eb11477f
Sarah Tomkovich <tomkoset@umich.edu>
2019-07-24 17:17:57 -0400

    Add alpha boxplots as Figure S2

---
+++ code/figure_s2.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(magick)
+library(cowplot)
+
+a <- ggdraw() + draw_image("results/figures/groups_shannon.png")
+b <- ggdraw() + draw_image("results/figures/groups_richness.png")
+
+plot_grid(a, b, labels = "AUTO", label_size = 12)+
+  ggsave("results/figures/figure_s2.pdf", width=6.5, height=3)+
+  ggsave("submission/figure_s2.pdf", width=6.5, height=3.5)
