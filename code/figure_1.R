4ddeedf23e99d49e4dd7bb8af10e3093c39ddc0c
Sarah Tomkovich <tomkoset@umich.edu>
2019-08-22 11:32:48 -0400

    Convert 1A to pdf so it will upload to GitHub

--- code/figure_1.R
+++ code/figure_1.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(magick)
 library(cowplot)
 
-a <- ggdraw() + draw_image("results/figures/PPI_Exp_Scheme.png")
+a <- ggdraw() + draw_image("results/figures/PPI_Exp_Scheme.pdf")
 b <- ggdraw() + draw_image("results/figures/before_C._diff_challenge.png")
 c <- ggdraw() + draw_image("results/figures/families_prev_assoc_w_PPIs_-7.png")
 d <- ggdraw() + draw_image("results/figures/families_prev_assoc_w_PPIs_0.png")
